05873e60ddeecff0723f8b7f020f7560330f11b3
shekhars-realm <sbhattacharya099@gmail.com>
2020-10-02 14:14:39 +0200

    resume updated

--- js/custom.js
+++ js/custom.js
@@ -329,7 +329,24 @@
 		portfolioMasonry();
 	});
 
-	
+	//Submit form
+
+	let fields = {}
+
+	var form = document.getElementById('form')
+
+	fields.firstName = document.getElementById('firstName');
+	fields.lastName = document.getElementById('lastName');
+	fields.email = document.getElementById('email');
+	fields.address = document.getElementById('address');
+	fields.houseNumber = document.getElementById('houseNumber');
+	fields.country = document.getElementById('country');
+	fields.password = document.getElementById('password');
+	fields.passwordCheck = document.getElementById('passwordCheck');
+	fields.newsletter = document.getElementById('newsletter');
+	fields.question = document.getElementById('question');
+
+	console.log(fields)
 
 
 })();
